b800d33d6a0c076207b0720c148af099ba48587e
Git-huk <kentmurley8@gmail.com>
2025-06-19 17:59:36 +0100

    Update anti-call.js

--- plugins/anti-call.js
+++ plugins/anti-call.js
@@ -1,9 +1,32 @@
 const { cmd } = require('../command');
 const config = require('../config');
 
+cmd({
+    pattern: "anticall",
+    alias: ["nocall"],
+    desc: "Enable or disable nticall feature",
+    category: "settings",
+    filename: __filename
+},    
+async (conn, mek, m, { from, args, isCreator, reply }) => {
+    if (!isCreator) return reply("*📛 ᴏɴʟʏ ᴛʜᴇ ᴏᴡɴᴇʀ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ!*");
+
+    const status = args[0]?.toLowerCase();
+    // Default value for ANTI_CALL is "false"
+    if (args[0] === "on") {
+        config.ANTI_CALL = "true";
+        return reply("Anti-call is now enabled.");
+    } else if (args[0] === "off") {
+        config.ANTI_CALL = "false";
+        return reply("Anti-call is now disabled.");
+    } else {
+        return reply(`*🫟 ᴇxᴀᴍᴘʟᴇ:  ${config.PREFIX}anticall on*`);
+    }
+}); 
+
 
 cmd({
-     on:"body"},async(conn, mek, m, {from, body, isCmd,isGroup,isOwner,isAdmins,groupAdmins,isBotAdmins,sender,groupName,quoted})=>{
+     on:"body"},async(conn, mek, m, {from, body, isGroup, isOwner, isAdmins, sender, quoted })=>{
 try{
 conn.ev.on("call", async(json) => {
 	  if(config.ANTI_CALL === "true") { 
