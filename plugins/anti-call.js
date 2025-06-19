2d015f916555c1088b7609dec453d27edd84ae11
Git-huk <kentmurley8@gmail.com>
2025-06-19 22:36:45 +0100

    Update anti-call.js

--- plugins/anti-call.js
+++ plugins/anti-call.js
@@ -25,27 +25,5 @@ async (conn, mek, m, { from, args, isCreator, reply }) => {
 }); 
 
 
-cmd({
-     on:"body"},async(conn, mek, m, {from, body, isGroup, isOwner, reply, sender, quoted })=>{
-try{
-conn.ev.on("call", async(json) => {
-	  if(config.ANTI_CALL === "true") { 
-    	for(const id of json) {
-    		if(id.status == "offer") {
-    			if(id.isGroup == false) {
-    				await conn.rejectCall(id.id, id.from);
-				
-				if ( mek.key.fromMe) return;
-	
-    			} else {
-    				await conn.rejectCall(id.id, id.from);
-    			}
-    		}
-    	}}
-    });
-} catch (e) {
-console.log(e)
-reply(e)
-}}
-)
+
 
